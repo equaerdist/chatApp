d9d574865d798f233212d315e0c724f2eb4c28d3
equaer <vriyazzz1001@gmail.com>
2023-07-15 16:58:58 +0300

    add error messages to Attributes

--- Dto/AddUserDto.cs
+++ Dto/AddUserDto.cs
@@ -4,13 +4,13 @@ namespace WebApplication5.Dto
 {
     public class AddUserDto 
     {
-        [MinLength(5)]
-        [Required, MaxLength(135)]
+        [MinLength(5, ErrorMessage = "Полное имя не может быть короче 5 символов")]
+        [Required(ErrorMessage = "Поле является обязательным"), MaxLength(135, ErrorMessage = "Максимальная длина для полного имени не больше 135 символов")]
         public string FullName {get; set;} = null!;
-        [MaxLength(100), Required]
-        [MinLength(3)]
+        [MaxLength(100, ErrorMessage = "Ник не должен превышать 100 символов"), Required(ErrorMessage = "Поле является обязательным")]
+        [MinLength(3, ErrorMessage = "Ник не должен быть короче 3 символов")]
         public string Nickname {get;set; } = null!;
-        [MinLength(8)]
+        [MinLength(8, ErrorMessage = "Пароль не должен быть короче 8 символов")]
         public string Password {get;set;} = null!;
     }
 }
